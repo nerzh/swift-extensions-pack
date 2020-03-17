13e9b7e7f9a6ffcbbb2551cb2457a94486442e8d
OleG <emptystamp@gmail.com>
2020-03-17 03:09:34 +0200

    public ...

--- Sources/SwiftExtensionsPack/Dictionary/Dictionary.swift
+++ Sources/SwiftExtensionsPack/Dictionary/Dictionary.swift
@@ -2,12 +2,12 @@ import Foundation
 
 extension Dictionary {
 
-    func toJSON(options: JSONSerialization.WritingOptions = []) throws -> String? {
+    public func toJSON(options: JSONSerialization.WritingOptions = []) throws -> String? {
         let data = try JSONSerialization.data(withJSONObject: self, options: options)
         return String(data: data, encoding: .utf8)
     }
 
-    func toJSONData(options: JSONSerialization.WritingOptions = []) throws -> Data {
+    public func toJSONData(options: JSONSerialization.WritingOptions = []) throws -> Data {
         try JSONSerialization.data(withJSONObject: self, options: options)
     }
 }
