65d59ebb9fe34bd9aceb0e206ad408e611c7bed0
nerzh <emptystamp@gmail.com>
2022-06-30 06:37:04 +0200

    add AnyValue

--- Sources/SwiftExtensionsPack/String/String+JSON.swift
+++ Sources/SwiftExtensionsPack/String/String+JSON.swift
@@ -15,4 +15,15 @@ extension String {
         guard let data = self.data(using: .utf8) else { return nil }
         return try? JSONDecoder().decode(model, from: data)
     }
+    
+    func toDictionary() -> [String: Any]? {
+        if let data = self.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
 }
