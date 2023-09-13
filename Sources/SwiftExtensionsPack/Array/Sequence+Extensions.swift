9fc8b00ce1c7af9d7e1cbd085f1a9e5d38ee3231
nerzh <emptystamp@gmail.com>
2023-09-13 20:44:37 +0200

    add uniq

--- Sources/SwiftExtensionsPack/Array/Sequence+Extensions.swift
+++ Sources/SwiftExtensionsPack/Array/Sequence+Extensions.swift
@@ -13,3 +13,17 @@ extension Sequence where Element: LosslessStringConvertible {
         return String(n.dropLast(separator.count))
     }
 }
+
+extension Sequence where Element: Hashable {
+    func uniq() -> Self {
+        return Array(Set(self)) as! Self
+    }
+}
+
+extension Sequence {
+    func uniq<T: Hashable>(_ by: (Element) -> T) -> Self {
+        var uniqueValues: [T: Element] = .init()
+        forEach { uniqueValues[by($0)] = $0 }
+        return uniqueValues.values.map { $0 } as! Self
+    }
+}
