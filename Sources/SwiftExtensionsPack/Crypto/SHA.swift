e92fa7abce7b687b819ad907770c099b0283f248
nerzh <emptystamp@gmail.com>
2024-03-14 00:37:10 +0100

    add SHA

---
+++ Sources/SwiftExtensionsPack/Crypto/SHA.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  
+//
+//  Created by Oleh Hudeichuk on 14.03.2024.
+//
+
+import Foundation
+#if os(Linux)
+import Crypto
+#else
+import CryptoKit
+#endif
+
+public extension SEPCrypto {
+    enum SHA {
+        case sha256
+        case sha384
+        case sha512
+        
+        func digest(data: Data) -> any Digest {
+            switch self {
+            case .sha256:
+                SHA256.hash(data: data)
+            case .sha384:
+                SHA384.hash(data: data)
+            case .sha512:
+                SHA512.hash(data: data)
+            }
+        }
+    }
+}
