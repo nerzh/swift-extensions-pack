c1dcbc433f70d46cfcb2d02b4cc0aedf9b5659b0
nerzh <emptystamp@gmail.com>
2024-03-17 16:09:19 +0100

    fix random

--- Sources/SwiftExtensionsPack/Crypto/RandomBytes.swift
+++ Sources/SwiftExtensionsPack/Crypto/RandomBytes.swift
@@ -12,26 +12,12 @@ public func randomBytes(count: UInt) -> [UInt8] {
     return (0..<count).map({ _ in UInt8.random(in: 0...UInt8.max, using: &generator) })
 }
 
-public func randomNumber(min: UInt, max: UInt) -> UInt {
-    let range = max - min
-    var bitsNeeded = UInt(ceil(log2(Double(range))))
-    bitsNeeded = bitsNeeded == 0 ? 1 : bitsNeeded
-    let bytesNeeded = UInt(ceil(Double(bitsNeeded) / 8.0))
-    let mask: UInt = bitsNeeded >= 64 ? UInt.max : (1 << bitsNeeded) - 1
-    
-    while true {
-        var numberValue: UInt = 0
-        let res = randomBytes(count: bytesNeeded)
-        var power = (bytesNeeded - 1) * 8
-        power += 8
-        for byte in res {
-            power -= 8
-            numberValue += UInt(byte) * (1 << power)
-        }
-        
-        numberValue &= mask
-        if numberValue < range {
-            return min + numberValue
-        }
-    }
+public func randomUInt(min: UInt, max: UInt) -> UInt {
+    var generator = SystemRandomNumberGenerator()
+    return UInt.random(in: min...max, using: &generator)
+}
+
+public func randomInt(min: Int, max: Int) -> Int {
+    var generator = SystemRandomNumberGenerator()
+    return Int.random(in: min...max, using: &generator)
 }
